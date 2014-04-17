5c43da56f13546bb2b38e8d11b3004e3e007da02
Abhi Aiyer <abhiaiyer91@gmail.com>
2014-04-16 21:50:35 -0700

    push back

--- app/scripts/directives/onePageNav.js
+++ app/scripts/directives/onePageNav.js
@@ -1,5 +1,27 @@
 'use strict';
 
+app.directive('slide', function (){
+	return {
+    	link: function(scope, element, attrs){
+			element.revolution({
+				delay:9000,
+				startwidth:950,
+				startheight:550,
+				onHoverStop:"on",
+				navigationType:"none",
+				soloArrowLeftHOffset:0,
+				soloArrowLeftVOffset:0,
+				soloArrowRightHOffset:0,
+				soloArrowRightVOffset:0,
+				touchenabled:"on",
+				fullWidth:"on",
+				shadow:0
+			});
+
+    }
+};
+});
+
 app.directive('activate', function (){
   return {
     link: function(scope, element, attrs ) {
